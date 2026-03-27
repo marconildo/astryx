13d7b1c95107e29bdb1f1256a4004a8f4eb3b5b3
rubyycheung <254546175+rubyycheung@users.noreply.github.com>
2026-03-27 09:13:14 -0500

    feat(sandbox): build status notification button in top nav (#922)

--- apps/sandbox/src/app/SandboxTopNav.tsx
+++ apps/sandbox/src/app/SandboxTopNav.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import {XDSTopNav, XDSTopNavHeading} from '@xds/core/TopNav';
+import {BuildStatus} from './BuildStatus';
 
 export function SandboxTopNav() {
   return (
     <XDSTopNav
       label="Main navigation"
       heading={<XDSTopNavHeading heading="XDS Sandbox" />}
+      endContent={<BuildStatus />}
     />
   );
 }
